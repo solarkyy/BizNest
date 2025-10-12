96d40e1639ef897b1221fa14590c125fbbb5af62
solarkyy <90358006+solarkyy@users.noreply.github.com>
2025-10-12 17:55:17 -0400

    Add S3-style stylesheet and app.js; remove Twitter link (s3-copy)

---
+++ app.js
@@ -0,0 +1,7 @@
+// app.js
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
